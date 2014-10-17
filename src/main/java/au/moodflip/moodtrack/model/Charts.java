1d7f62c2f50cb52f35665a7254fde0a5de094433
Farhana Chowdhury <rafsin@192-168-1-6.tpgi.com.au>
2014-10-18 00:50:06 +1100

    Added Chart class in data model.

---
+++ src/main/java/au/moodflip/moodtrack/model/Charts.java
@@ -0,0 +1,50 @@
+package au.moodflip.moodtrack.model;
+
+import au.moodflip.personalisation.model.User;
+
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Charts implements Serializable {
+
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    private Date startDate;
+
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    private Date endDate = new Date();
+
+    private User user;
+
+    public Charts(User user) {
+        this.user = user;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
+
+
