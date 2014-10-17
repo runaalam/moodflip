09020315d5fe9bdef3b0e9ad4457d173636d70f0
Farhana Chowdhury <rafsin@192-168-1-6.tpgi.com.au>
2014-10-18 00:03:52 +1100

    Changed name of DataDaoImp to "DataDaoImpl".

---
+++ src/main/java/au/moodflip/moodtrack/dao/DataDaoImpl.java
@@ -0,0 +1,58 @@
+package au.moodflip.moodtrack.dao;
+
+import java.util.List;
+
+
+import au.moodflip.moodtrack.model.ReportCmd;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import au.moodflip.moodtrack.model.Data;
+
+@SuppressWarnings("unchecked")
+@Repository
+public class DataDaoImpl implements DataDao {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public void save(Data data) {
+        sessionFactory.getCurrentSession().save(data);
+    }
+
+    @Override
+    public Data update(Data data) {
+        return (Data) sessionFactory.getCurrentSession().merge(data);
+    }
+
+    @Override
+    public List<Data> listData(ReportCmd reportCmd) {
+        return sessionFactory.getCurrentSession().createQuery("SELECT d FROM Data d" +
+                " WHERE d.user = :user" +
+                " AND d.date BETWEEN :startDate AND :endDate")
+                .setParameter("user", reportCmd.getUser())
+                .setParameter("startDate", reportCmd.getStartDate())
+                .setParameter("endDate", reportCmd.getEndDate())
+                .list();
+    }
+
+    @Override
+    public Data findData(Data data) {
+        List<Data> list = sessionFactory.getCurrentSession().createQuery("SELECT d FROM Data d" +
+                " WHERE d.user = :user" +
+                " AND d.date = :date")
+                .setParameter("user", data.getUser())
+                .setParameter("date", data.getDate())
+                .list();
+
+        if (list != null && !list.isEmpty()) {
+            return
+                    list.get(0);
+        }
+
+        return null;
+    }
+
+}
