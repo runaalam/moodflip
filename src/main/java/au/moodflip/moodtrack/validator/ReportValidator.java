5ab05f563f0a2b9421f9bb3528adabfc0cd5c2f7
Farhana Chowdhury <rafsin@192-168-1-6.tpgi.com.au>
2014-10-17 23:52:09 +1100

    Added ReportValidator.

---
+++ src/main/java/au/moodflip/moodtrack/validator/ReportValidator.java
@@ -0,0 +1,43 @@
+package au.moodflip.moodtrack.validator;
+
+import au.moodflip.moodtrack.model.ReportCmd;
+import org.joda.time.DateTime;
+import org.joda.time.Days;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import java.util.Date;
+
+@Component
+public class ReportValidator implements Validator {
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return ReportCmd.class.isAssignableFrom(aClass);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        ReportCmd reportCmd = (ReportCmd) o;
+
+        if (reportCmd.getStartDate() == null) {
+            errors.reject("startDate", "start.date.required");
+        }
+
+        Date endDate = reportCmd.getEndDate() != null ? reportCmd.getEndDate() : new Date();
+
+        if (endDate.before(reportCmd.getStartDate())) {
+            errors.reject("start.date.after.end");
+        }
+
+        int reportRange = Days
+                .daysBetween(new DateTime(reportCmd.getStartDate()), new DateTime(endDate))
+                .getDays();
+
+        if (reportRange < 7) {
+            errors.reject("report.date.range");
+        }
+
+    }
+}
