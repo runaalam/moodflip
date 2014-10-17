b01584f7867386154650b6cbdfdcb6b803e757ad
Farhana Chowdhury <rafsin@192-168-1-6.tpgi.com.au>
2014-10-18 00:05:06 +1100

    Modified DataDao class.

--- src/main/java/au/moodflip/moodtrack/dao/DataDao.java
+++ src/main/java/au/moodflip/moodtrack/dao/DataDao.java
@@ -2,10 +2,8 @@ package au.moodflip.moodtrack.dao;
 
 import java.util.List;
 
-
-
-
 import au.moodflip.moodtrack.model.Data;
+import au.moodflip.moodtrack.model.ReportCmd;
 
 public interface DataDao {
 
@@ -13,8 +11,8 @@ public interface DataDao {
 
     public Data update(Data data) ;
     
-    public List<Data> listData();
+    public List<Data> listData(ReportCmd reportCmd);
 
 
-
+    Data findData(Data data);
 }
