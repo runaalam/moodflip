2ccde7d0c5f61891d4aefbe972205ebc5c0f6119
Your Name <you@example.com>
2014-10-25 21:47:14 +1100

    Added mood-tracking data in the "My Activity" field. Also modified date validations for reports and data pages.

--- src/main/java/au/moodflip/moodtrack/controller/DataController.java
+++ src/main/java/au/moodflip/moodtrack/controller/DataController.java
@@ -3,12 +3,13 @@ package au.moodflip.moodtrack.controller;
 import au.moodflip.moodtrack.service.DataService;
 
 
-
 import au.moodflip.moodtrack.model.Data;
 
 import au.moodflip.moodtrack.validator.DataValidator;
 import au.moodflip.personalisation.model.User;
 import au.moodflip.personalisation.service.UserManager;
+import au.moodflip.userpage.model.Activity;
+import au.moodflip.userpage.service.ActivityService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +28,7 @@ import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import java.security.Principal;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 
@@ -44,6 +46,9 @@ public class DataController {
     @Autowired
     private DataService dataService;
 
+    @Autowired
+    private ActivityService activityService;
+
     @RequestMapping(value = "/data", method = RequestMethod.GET)
     public String data(Principal principal,
                        Model model) {
@@ -56,6 +61,7 @@ public class DataController {
 
     @RequestMapping(value = "/data", method = RequestMethod.POST)
     public ModelAndView saveData(@ModelAttribute("command") Data data,
+                                 Principal principal,
                                  BindingResult result,
                                  Model model) {
 
@@ -65,9 +71,11 @@ public class DataController {
         }
 
         Data oldData = dataService.findData(data);
+        String activityDesc = null;
 
         if (oldData == null) {
             dataService.save(data);
+            activityDesc = "User has entered mood data for";
         } else {
             if (data.getId() != oldData.getId()) {
                 model.addAttribute("command", oldData);
@@ -75,8 +83,17 @@ public class DataController {
                 return new ModelAndView(FOLDER + "/data");
             } else {
                 dataService.update(data);
+                activityDesc = "User has updated mood data for";
             }
         }
+
+        User user = userManager.getUserByUsername(principal.getName());
+
+        Activity activity = new Activity();
+        activity.setUser(user);
+        activity.setDescription(activityDesc);
+        activity.setActivityDate(data.getDate());
+        activityService.addActivity(activity);
 
         return new ModelAndView(new RedirectView("/mood-tracking", true));
     }
